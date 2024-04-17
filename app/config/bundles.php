4a3b34d9a0d8b95faa7a7c068e3edcc2cae0c993
Vladyslav Drybas <ukdvvw@gmail.com>
2024-04-17 19:20:38 +0200

    add oauth user connect for github

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
 ];
---
+++ app/src/Controller/OAuth/GithubController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller\OAuth;
+
+use JetBrains\PhpStorm\NoReturn;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use KnpU\OAuth2ClientBundle\Client\Provider\GithubClient;
+use App\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+#[Route('/oauth/github', name: "oauth_github")]
+class GithubController extends AbstractController
+{
+    /**
+     * Link to this controller to start the "connect" process
+     */
+    #[Route("/connect", name: "_connect", methods: ["GET"])]
+    public function connectAction(ClientRegistry $clientRegistry): RedirectResponse
+    {
+        // will redirect to Facebook!
+        return $clientRegistry
+            ->getClient('github') // key used in config/packages/knpu_oauth2_client.yaml
+            ->redirect([
+                'user',
+                'repo', // the scopes you want to access
+            ]);
+    }
+
+    /**
+     * After going to Github, you're redirected back here
+     * because this is the "redirect_route" you configured
+     * in config/packages/knpu_oauth2_client.yaml
+     */
+    #[NoReturn] #[Route("/connect/check", name: "_connect_check", methods: ["GET"])]
+    public function  connectCheckAction(Request $request, ClientRegistry $clientRegistry): JsonResponse
+    {
+        // ** if you want to *authenticate* the user, then
+        // leave this method blank and create a Guard authenticator
+        // (read below)
+
+        /** @var GithubClient $client */
+        $client = $clientRegistry->getClient('github');
+
+        $token = $client->getAccessToken([
+            'user', 'repo:status' // the scopes you want to access
+        ]);
+
+        // TODO Store token to db
+
+        return $this->json([
+            'message' => 'success',
+//            'token' => $token->getToken(),
+        ], Response::HTTP_OK);
+    }
+}
